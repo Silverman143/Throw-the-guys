8e3ed9de1185db89704fc2b4f888fe33301633ab
Silverman143 <luckbrovova@gmail.com>
2022-12-22 11:08:41 +0300

    no LineStudioAnalitic

---
+++ Assets/Settings.cs
@@ -0,0 +1,75 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Firebase;
+using com.adjust.sdk;
+
+public class Settings : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        FireBaseSetter();
+        AppLovinEnabled();
+
+#if UNITY_IOS
+        /* Mandatory - set your iOS app token here */
+        InitAdjust("YOUR_IOS_APP_TOKEN_HERE");
+#elif UNITY_ANDROID
+        /* Mandatory - set your Android app token here */
+        InitAdjust("dsnh9ut1wagw");
+#endif
+    }
+
+    private void FireBaseSetter()
+    {
+        Firebase.FirebaseApp.CheckAndFixDependenciesAsync().ContinueWith(task => {
+            var dependencyStatus = task.Result;
+            if (dependencyStatus == Firebase.DependencyStatus.Available)
+            {
+                // Create and hold a reference to your FirebaseApp,
+                // where app is a Firebase.FirebaseApp property of your application class.
+                var app = Firebase.FirebaseApp.DefaultInstance;
+
+                // Set a flag here to indicate whether Firebase is ready to use by your app.
+                Debug.Log("Firebase works");
+            }
+            else
+            {
+                UnityEngine.Debug.LogError(System.String.Format(
+                  "Could not resolve all Firebase dependencies: {0}", dependencyStatus));
+                // Firebase Unity SDK is not safe to use here.
+            }
+        });
+    }
+
+    private void AppLovinEnabled()
+    {
+
+        MaxSdk.SetSdkKey("cRZIqX9VfJHnhXT9hJ4AQR4KreexXQnUwOnfEN4eTT0cMq2QKS7H6b6d1ypnCzwxrEI7eO9uofb5_ArUvWZ2oq");
+        MaxSdk.SetUserId(SystemInfo.deviceUniqueIdentifier);
+        MaxSdk.SetVerboseLogging(true);
+        MaxSdk.InitializeSdk();
+    }
+
+    private void InitAdjust(string adjustAppToken)
+    {
+        var adjustConfig = new AdjustConfig(
+            adjustAppToken,
+            AdjustEnvironment.Production, // AdjustEnvironment.Sandbox to test in dashboard
+            true
+        );
+        adjustConfig.setLogLevel(AdjustLogLevel.Info); // AdjustLogLevel.Suppress to disable logs
+        adjustConfig.setSendInBackground(true);
+        new GameObject("Adjust").AddComponent<Adjust>(); // do not remove or rename
+
+        // Adjust.addSessionCallbackParameter("foo", "bar"); // if requested to set session-level parameters
+
+        //adjustConfig.setAttributionChangedDelegate((adjustAttribution) => {
+        //  Debug.LogFormat("Adjust Attribution Callback: ", adjustAttribution.trackerName);
+        //});
+
+        Adjust.start(adjustConfig);
+
+    }
+}
